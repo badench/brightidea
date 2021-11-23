ae79e4dfbca19d70aab10381a4192a90c0e7de73
Connor Bade <cbade@proofpoint.com>
2021-11-22 20:22:04 -0800

    rooms work now. Implemented as a Hashmap for now. Perhaps later it would be more appropriate to organize them as a struct as they need to hold more data. Main challenge to overcome with that approach is lookup, as right now its O(1) with Hashmap. Also some cleanup.

--- src/handlers/chat_handlers.rs
+++ src/handlers/chat_handlers.rs
@@ -1,16 +1,34 @@
-use std::sync::atomic::Ordering;
-use tokio::sync::mpsc;
+use std::sync::{atomic::{AtomicUsize, Ordering}, Arc};
+use std::collections::HashMap;
+use tokio::sync::{mpsc, RwLock};
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use futures_util::{SinkExt, StreamExt, TryFutureExt};
 use warp::ws::{Message, WebSocket};
-use warp::Filter;
-use crate::{NEXT_USER_ID, Users};
 
-pub async fn user_connected(ws: WebSocket, users: Users) {
+/// Our global unique user id counter.
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
+
+/// Our state of currently connected users.
+///
+/// - Key is their id
+/// - Value is a sender of `warp::ws::Message`
+type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Message>>>>;
+pub type Rooms = Arc<RwLock<HashMap<String, Users>>>;
+
+pub async fn join_room(ws: WebSocket, room_name: String, rooms: Rooms) {
+    //Acquire write guard to get map of users and then update
+    let mut room_guard = rooms.write().await;
+    let users = match room_guard.get(&room_name).clone() {
+        Some(users) => {
+            users.clone()
+        },
+        None => {
+            Users::default()
+        }
+    };
     // Use a counter to assign a new unique ID for this user.
     let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
 
-    eprintln!("new chat user: {}", my_id);
 
     // Split the socket into a sender and receive of messages.
     let (mut user_ws_tx, mut user_ws_rx) = ws.split();
@@ -33,6 +51,8 @@ pub async fn user_connected(ws: WebSocket, users: Users) {
 
     // Save the sender in our list of connected users.
     users.write().await.insert(my_id, tx);
+    room_guard.insert(room_name.clone(), users);
+    drop(room_guard);
 
     // Return a `Future` that is basically a state machine managing
     // this specific user's connection.
@@ -47,15 +67,15 @@ pub async fn user_connected(ws: WebSocket, users: Users) {
                 break;
             }
         };
-        user_message(my_id, msg, &users).await;
+        user_message(my_id, msg, &room_name, &rooms).await;
     }
 
     // user_ws_rx stream will keep processing as long as the user stays
     // connected. Once they disconnect, then...
-    user_disconnected(my_id, &users).await;
+    user_disconnected(my_id, &room_name, &rooms).await;
 }
 
-async fn user_message(my_id: usize, msg: Message, users: &Users) {
+async fn user_message(my_id: usize, msg: Message, room_name: &str, rooms: &Rooms) {
     // Skip any non-Text messages...
     let msg = if let Ok(s) = msg.to_str() {
         s
@@ -65,21 +85,25 @@ async fn user_message(my_id: usize, msg: Message, users: &Users) {
 
     let new_msg = format!("<User#{}>: {}", my_id, msg);
 
-    // New message from this user, send it to everyone else (except same uid)...
-    for (&uid, tx) in users.read().await.iter() {
-        if my_id != uid {
-            if let Err(_disconnected) = tx.send(Message::text(new_msg.clone())) {
-                // The tx is disconnected, our `user_disconnected` code
-                // should be happening in another task, nothing more to
-                // do here.
+    if let Some(users) = rooms.read().await.get(room_name) {
+        // New message from this user, send it to everyone else (except same uid)...
+        for (&uid, tx) in users.read().await.iter() {
+            if my_id != uid {
+                if let Err(_disconnected) = tx.send(Message::text(new_msg.clone())) {
+                    // The tx is disconnected, our `user_disconnected` code
+                    // should be happening in another task, nothing more to
+                    // do here.
+                }
             }
         }
     }
 }
 
-async fn user_disconnected(my_id: usize, users: &Users) {
+async fn user_disconnected(my_id: usize, room_name: &str, rooms: &Rooms) {
     eprintln!("good bye user: {}", my_id);
 
     // Stream closed up, so remove from the user list
-    users.write().await.remove(&my_id);
+    if let Some(users) = rooms.read().await.get(room_name) {
+        users.write().await.remove(&my_id);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,33 @@
 // #![deny(warnings)]
-use std::collections::HashMap;
-use std::sync::{
-    atomic::{AtomicUsize},
-    Arc,
-};
-
-use tokio::sync::{mpsc, RwLock};
-use warp::ws::{Message};
 use warp::Filter;
 
 mod handlers;
+use handlers::chat_handlers::Rooms;
 
-/// Our global unique user id counter.
-static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
-
-/// Our state of currently connected users.
-///
-/// - Key is their id
-/// - Value is a sender of `warp::ws::Message`
-type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Message>>>>;
 
 //TODO:
-// 1. Input room Id
-// 2. click "Enter Room" which changes messages from "Connecting" to "Connected to room#" and establishes websocket
-// 3. adjust route for message sending to be /chat/room
 // 4. Create struct to model 'room'
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    // Keep track of all connected users, key is usize, value
-    // is a websocket sender.
-    let users = Users::default();
+    //Keep track of all rooms, key is a string representing a room, value
+    // is a Users Map
+    let rooms = Rooms::default();
+
     // Turn our "state" into a new Filter...
-    let users = warp::any().map(move || users.clone());
+    let rooms = warp::any().map(move || rooms.clone());
 
     // GET /chat -> websocket upgrade
     let chat = warp::path!("chat" / String)
         // The `ws()` filter will prepare Websocket handshake...
         .and(warp::ws())
-        .and(users)
-        .map(|room_name, ws: warp::ws::Ws, users| {
+        .and(rooms)
+        .map(|room_name: String, ws: warp::ws::Ws, rooms: Rooms| {
             // This will call our function if the handshake succeeds.
             eprintln!("room Id {}", room_name);
-            ws.on_upgrade(move |socket| handlers::chat_handlers::user_connected(socket, users))
+            ws.on_upgrade(move |socket| handlers::chat_handlers::join_room(socket, room_name, rooms))
         });
 
     // GET / -> index html
